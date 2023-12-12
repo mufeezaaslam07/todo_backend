cb0da3a7ca8e83b30fa87fd06fe745d3f10d8ee2
mufeezaaslam07 <mufeeza_aslam_intern@netixsol.com>
2023-12-12 18:19:10 +0500

    todo backend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3500;
+
+const TodoItemRoute = require("./routes/todoItems");
+
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", TodoItemRoute);
+
+app.listen(PORT, () => console.log("Server connected"));
